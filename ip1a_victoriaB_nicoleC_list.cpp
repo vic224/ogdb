e49a8a0fc9d788e8542be849114953b03b114628
vic224 <46499514+vic224@users.noreply.github.com>
2019-01-08 11:28:47 -0800

    Create ip1a_victoriaB_nicoleC_list.cpp

---
+++ ip1a_victoriaB_nicoleC_list.cpp
@@ -0,0 +1,35 @@
+/*
+    Filename: ip1a_victoriaB_nicoleC_list.cpp
+    Description: Program to print a list using only cout and endl
+    Driver: Victoria Binkley
+    Navigator: Nicole Covey
+    Date: 1/8/2019 
+
+*/
+ 
+// preprocessor directives
+
+
+#include <iostream> //cout
+using namespace std;
+
+// main function
+int main()
+{
+   cout << "Animals"<< endl << endl;
+   cout << "Penguin" << endl
+        << "Zebra" << endl
+        << "Cat" << endl
+        << "Dog" << endl
+        << "Human" << endl
+        << "Lion" << endl;
+   cout << "Shark" << endl;
+   cout << "Starfish" << endl;
+   cout << "Tiger" << endl;
+   cout << "Parrot";
+    
+    
+    
+    return 0;
+    
+}
